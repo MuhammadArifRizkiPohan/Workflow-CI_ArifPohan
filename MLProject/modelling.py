69d2f4593895b16e43ac2f9d4aa169b14f425ebb
MuhammadArifRizki Pohan <arifpohan127@gmail.com>
2025-12-24 21:14:01 +0700

    Add MLflow CI

---
+++ MLProject/modelling.py
@@ -0,0 +1,36 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, f1_score
+
+import mlflow
+import mlflow.sklearn
+
+# Aktifkan autolog
+mlflow.sklearn.autolog()
+
+def train_model():
+    # Load dataset preprocessing
+    df = pd.read_csv("heartdisease_preprocessing.csv")
+
+    X = df.drop("target", axis=1)
+    y = df["target"]
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=42
+    )
+
+    model = LogisticRegression(max_iter=1000)
+
+    with mlflow.start_run():
+        model.fit(X_train, y_train)
+
+        y_pred = model.predict(X_test)
+        acc = accuracy_score(y_test, y_pred)
+        f1 = f1_score(y_test, y_pred)
+
+        print("Accuracy:", acc)
+        print("F1 Score:", f1)
+
+if __name__ == "__main__":
+    train_model()
